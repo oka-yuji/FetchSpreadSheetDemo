c132487a91e18d1a73a3c54a2ef5382b2af2d46f
岡優志 <yu.j.0513@gmail.com>
2023-04-27 23:30:19 +0900

    first commit

---
+++ FetchSpreadSheetDemo/FetchSpreadSheetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchSpreadSheetDemoApp.swift
+//  FetchSpreadSheetDemo
+//
+//  Created by yuji on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct FetchSpreadSheetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
